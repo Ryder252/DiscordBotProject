f79ec298411d769923058943f5c49f408ccc38f1
Ryder252 <shanebo2017@gmail.com>
2024-02-27 18:44:16 -0500

    delete

--- DiscordBot.py
+++ DiscordBot.py
@@ -2,39 +2,3 @@ import discord
 
 # Initialize the bot client
 client = discord.Client()
-
-
-hello its me shane
-4th edit 5th edit
-
-how are you
-hey shane
-help
-
-
-4th edit
-
-hey its elias;
-this is my second edit : this is izzy btw
-
-
-hey this is my edit from shane
-
-
-this is my second edit : this is izzy btw
-
-
-hey this is my edit from shane
-
-shane's message
-
-hey its elias 
-you all see this
-
-this is a cow
- whattuppppp its shane
-
-this is a BIGGER COW
-
-
-If this works then I am happy "izzy"
