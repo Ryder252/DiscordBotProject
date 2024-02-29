2381e1a526aee183a144a58ede756e0b467d0304
Ryder252 <shanebo2017@gmail.com>
2024-02-29 17:41:26 -0500

    wrlvno;rwvn

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,7 +1,15 @@
 import discord
 
-# Initialize the bot client
-client = discord.Client()
+def run_client():
+    TOKEN = 'MTIxMjg4MzExNjk3NzQzMDYxOQ.GQdplC.KJLpiQPaf7UZLaYP_o56zM8TGaxhxCb8whNL-U'
+    client = discord.Client()
+
+@client.event
+
+async def on_ready():
+    print(f'{client.user} is now running.')
+
+
 print("Hello world!")
 # Confirmed ideas: Quiz,
 
