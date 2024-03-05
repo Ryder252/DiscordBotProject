ccabb42bf5e025d3dc908f6f3a8f7b9c0935db32
Ryder252 <shanebo2017@gmail.com>
2024-03-04 22:52:19 -0500

    Further progress in connection.

--- bot.py
+++ bot.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+# TOKEN = 'MTIxMjg4MzExNjk3NzQzMDYxOQ.GcO2sA.fyM3h-NG2N-SqEiolzAvg_h9gat-yFAuhos5M'o
+client = discord.Client()
 
 @client.event
-def run_client():
-    # TOKEN = 'MTIxMjg4MzExNjk3NzQzMDYxOQ.GcO2sA.fyM3h-NG2N-SqEiolzAvg_h9gat-yFAuhos5M'o
-     client = discord.Client()
-     @client.event
-     async def on_ready():
-         print(f'{client.user} is now running.')
-     client.run(TOKEN)
+async def on_ready():
+    print(f'{client.user} is now running.')
+    
+client.run(TOKEN)
