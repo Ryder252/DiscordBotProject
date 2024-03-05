f2bcdecdb8408af4cd9725596be3de74e0760fd7
Ryder252 <shanebo2017@gmail.com>
2024-03-04 23:44:39 -0500

    Added some sample quote responses.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,7 +15,21 @@ async def on_ready():
         if guild.name == GUILD:
             break
     print(f'{client.user} is now connected to {guild.name}(ID: {guild.id})')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
     
+    dialogues = [
+        'Imagine a meeting between pre-Nazi Hitler and Bob Ross.',
+        'I wonder how the Revolutionary War would play out with the militaries of modern America and Great Britain.',
+        'Would hippies have existed as we know them today if America never went to Vietnam?'
+    ]    
+    if message.content == '!quote':
+        response = random.choice(dialogues)
+        await message.channel.send(response)    
+
 client.run(TOKEN)
 
 # Key features: 
